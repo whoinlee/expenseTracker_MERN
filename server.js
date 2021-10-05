216d06aca472be9ee3851373fa3fa6c0b7f5a550
Whoin Lee <whoinlee@gmail.com>
2021-10-05 16:30:31 -0400

    production env added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express'); //-- web framework
 const dotenv = require('dotenv');   //-- for global variables. loads environment variables from a .env file into process.env
 const morgan = require('morgan');   //-- HTTP request logger middleware for node.js
@@ -9,17 +10,23 @@ dotenv.config({path: './config/config.env'});
 connectDB();
 
 const app = express();
-//app.get('/', (req, res) => res.send('Hello WhoIN')); 
-
+app.use(express.json());    //-- allows to use bodyParser
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+//app.get('/', (req, res) => res.send('Hello WhoIN')); 
+
 //-- using router 
 const transactions = require('./routes/transactions');  //-- using express.Router
-
-app.use(express.json());    //-- allows to use bodyParser
 app.use('/api/v1/transactions', transactions);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    //-- '*':anything except for the api routes, '/api/v1/transactions'
+    //-- currentDir(__) >> client folder >> build folder >> index.html
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
