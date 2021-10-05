20f604d15726b339e5c83bd72f011e91f15b97ad
Whoin Lee <whoinlee@gmail.com>
2021-10-05 12:05:50 -0400

    client updated w. getTransactions

--- client/src/contexts/AppReducer.ts
+++ client/src/contexts/AppReducer.ts
@@ -1,9 +1,16 @@
 //-- Types
+// import AddTransaction from '../components/AddTransaction';
 import { State, Action } from '../datatypes/DataTypes';
 
 
 const AppReducer = (state:State, action:Action) => {
     switch(action.type) {
+      case 'GET_TRANSACTIONS':
+        return {
+          ...state,
+          loading: false,
+          transactions: action.payload
+        };
       case 'DELETE_TRANSACTION':
         return {
           ...state,
@@ -14,6 +21,11 @@ const AppReducer = (state:State, action:Action) => {
           ...state,
           transactions: [...state.transactions, action.payload]
         };
+      case 'TRANSACTION_ERROR':
+          return {
+            ...state,
+            error: action.payload
+          };
       default:
         return state;
     };
--- client/src/contexts/GlobalState.tsx
+++ client/src/contexts/GlobalState.tsx
@@ -2,16 +2,20 @@ import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 //-- Types
 import { TransactionType, State } from '../datatypes/DataTypes';
+import axios from 'axios';
 
 
 //-- Initial state
 const initialState:State = {
   transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
+    // { id: 1, text: 'Flower', amount: -20 },
+    // { id: 2, text: 'Salary', amount: 300 },
+    // { id: 3, text: 'Book', amount: -10 },
+    // { id: 4, text: 'Camera', amount: 150 }
   ],
+  error: null,
+  loading: true,
+  getTransactions: () => {},
   deleteTransaction: (id) => {},
   addTransaction: (transaction) => {}
 };
@@ -19,10 +23,30 @@ const initialState:State = {
 export const GlobalContext = createContext(initialState);
 
 
+const transaction_url = "/api/v1/transactions";
 type Props = { children?: React.ReactNode };
 //-- Provider component
 export const GlobalProvider = ({ children }:Props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  const getTransactions = async() => {
+    try {
+      const res = await axios.get(transaction_url);
+      // console.log("res:: ", res)
+      const dataObj:any = res.data;
+      const transactions = dataObj.data;
+      dispatch({
+        type: 'GET_TRANSACTIONS',
+        payload: transactions
+      })
+    } catch (err:any) {
+      dispatch({
+        type: 'TRANSACTION_ERROR',
+        payload: err.response.data.error
+      })
+    }
+  }
+  getTransactions();
   
   const deleteTransaction = (id:number) => {
     dispatch({
@@ -39,8 +63,11 @@ export const GlobalProvider = ({ children }:Props) => {
   };
 
   return (
-    <GlobalContext.Provider value={{transactions: state.transactions, deleteTransaction, addTransaction}} >
-        {children}
+    <GlobalContext.Provider value={{transactions: state.transactions, 
+                                    error: state.error, 
+                                    loading: state.loading, 
+                                    getTransactions, deleteTransaction, addTransaction}} >
+      {children}
     </GlobalContext.Provider>
   );
 };
